cb4adba710ac647ce83b92f6f58fb187b8fab373
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-21 16:30:09 +0530

    feat: update Avatar component to accept url and alt props, add type definition for those url and alt props

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -1,5 +1,12 @@
-const Avatar = () => {
-  return <img src="https://cataas.com/cat/says/hello%20world!" alt="@github-handle" />
+type AvatarProps = {
+    /** the url of an image to render for the Avatar */
+    url: string;
+    /** the alt text to use for the url image */
+    alt: string;
+  }
+
+const Avatar = ({url, alt}: AvatarProps) => {
+  return <img src={url} alt={alt} />
 }
 
 export default Avatar
