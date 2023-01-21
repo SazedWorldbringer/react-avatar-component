86a5919019face9915dc02cf2929bfe246072813
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-21 17:32:31 +0530

    feat: use cleanup to clean up the dom before each test, write tests to check if the Avatar component renders the fallback image if given url is broken/doesn't load and if given url is an empty string

--- src/Avatar.test.tsx
+++ src/Avatar.test.tsx
@@ -1,9 +1,13 @@
-import { describe, expect, it } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
 
 import Avatar, { FALLBACK_AVATAR_ALT_TEXT, FALLBACK_AVATAR_URL } from './Avatar'
 
 describe("Avatar", () => {
+    beforeEach(() => {
+        cleanup()
+    })
+
     it("should render an <img />", () => {
         const url = "https://cataas.com/cat/says/hello%20world";
         const alt = "@github-handle";
@@ -27,6 +31,22 @@ describe("Avatar", () => {
         render(<Avatar />)
 
         const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT)
+        expect(img).toHaveAttribute("src", FALLBACK_AVATAR_URL)
+    })
+
+    it("should use a fallback image if given image fails to load", () => {
+        render(<Avatar url="https://hello.com/fake.png" />)
+
+        const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT)
+        fireEvent.error(img)
+        expect(img).toHaveAttribute("src", FALLBACK_AVATAR_URL)
+    })
+
+    it("should use a fallback image if url is an empty string", () => {
+        render(<Avatar url="" />)
+
+        const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT)
+        fireEvent.error(img)
         expect(img).toHaveAttribute("src", FALLBACK_AVATAR_URL)
     })
 })
