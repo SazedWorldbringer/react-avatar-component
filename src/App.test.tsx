7ea4d87a4287c25acd694dd49ca501500e437e5b
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-21 18:00:22 +0530

    feat: update test to test the new title

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,6 +6,6 @@ import App from './App'
 describe("App", () => {
   it("should render with the title visible", () => {
     render(<App />)
-    expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
+    expect(screen.getByText(/Avatar Component using TypeScript and TDD using Vitest/i)).toBeInTheDocument();
   })
 })
