170805538455a9e7bf7360da52c9a22f16cee6e4
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-21 16:39:46 +0530

    feat: update test descriptions to be a more concise

--- src/Avatar.test.tsx
+++ src/Avatar.test.tsx
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react'
 import Avatar from './Avatar'
 
 describe("Avatar", () => {
-    it("should render an image with an alt text", () => {
+    it("should render an <img />", () => {
         const url = "https://cataas.com/cat/says/hello%20world";
         const alt = "@github-handle";
         render(<Avatar url={url} alt={alt}/>)
@@ -14,7 +14,7 @@ describe("Avatar", () => {
         expect(img.tagName).toBe("IMG")
     })
 
-    it("should render the url and alt passed in", () => {
+    it("should use the url and alt passed in", () => {
         const url = "https://cataas.com/cat/says/hello%20world";
         const alt = "a beautiful cat";
         render(<Avatar url={url} alt={alt} />)
