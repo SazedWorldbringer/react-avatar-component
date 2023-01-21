8525aeaed9c5d0a2eebf7b5b816a82622093c3ff
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-21 17:43:13 +0530

    feat: use state to keep track of the url prop as urlToRender, update urlToRender to fallback url when the onError event is fired

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
+
 export const FALLBACK_AVATAR_URL = "https://cataas.com/cat/says/hello%20world"
 export const FALLBACK_AVATAR_ALT_TEXT = "@hello-cat"
 
 type AvatarProps = {
-    /** the url of an image to render for the Avatar */
-    url?: string;
-    /** the alt text to use for the url image */
-    alt?: string;
-  }
+  /** the url of an image to render for the Avatar */
+  url?: string;
+  /** the alt text to use for the url image */
+  alt?: string;
+}
 
-const Avatar = ({url = FALLBACK_AVATAR_URL, alt = FALLBACK_AVATAR_ALT_TEXT}: AvatarProps) => {
-  return <img src={url} alt={alt} />
+const Avatar = ({ url = FALLBACK_AVATAR_URL, alt = FALLBACK_AVATAR_ALT_TEXT }: AvatarProps) => {
+  const [srcToRender, setSourceToRender] = useState(url)
+  return <img src={srcToRender} alt={alt} onError={() => setSourceToRender(FALLBACK_AVATAR_URL)} />
 }
 
 export default Avatar
