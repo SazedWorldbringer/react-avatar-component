3b5a0f1775136ea96ef88d1769eeb2c5aada854c
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-21 17:14:57 +0530

    feat: make props optional, define variables for fallback url and alt and export them, pass default values to Avatar component

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -1,11 +1,14 @@
+export const FALLBACK_AVATAR_URL = "https://cataas.com/cat/says/hello%20world"
+export const FALLBACK_AVATAR_ALT_TEXT = "@hello-cat"
+
 type AvatarProps = {
     /** the url of an image to render for the Avatar */
-    url: string;
+    url?: string;
     /** the alt text to use for the url image */
-    alt: string;
+    alt?: string;
   }
 
-const Avatar = ({url, alt}: AvatarProps) => {
+const Avatar = ({url = FALLBACK_AVATAR_URL, alt = FALLBACK_AVATAR_ALT_TEXT}: AvatarProps) => {
   return <img src={url} alt={alt} />
 }
 
